d5c152d69b884180f3ef297a1827427b536f1a23
Gianpiero <gianpierotulipani@outlook.it>
2021-01-22 15:21:01 +0100

    Card con XML contapassi e dettaglio

--- app/src/main/java/com/example/walktoshop/User/CardView.java
+++ app/src/main/java/com/example/walktoshop/User/CardView.java
@@ -2,6 +2,7 @@ package com.example.walktoshop.User;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -10,13 +11,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.walktoshop.R;
 
 public class CardView extends AppCompatActivity {
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_view);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
+        progressBar = (ProgressBar) findViewById(R.id.progerssBar);
+        progressBar.setProgress(70);
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if(item.getItemId() == android.R.id.home){
